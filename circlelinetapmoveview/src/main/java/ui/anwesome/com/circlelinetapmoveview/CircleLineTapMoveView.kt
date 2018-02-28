dee54ca9e763a3daab1d118cb10ff2b42da7f72e
Anwesh43 <anweshthecool0@gmail.com>
2018-03-01 01:21:50 +0530

    adding CircleLineTapMoveView classs

--- circlelinetapmoveview/src/main/java/ui/anwesome/com/circlelinetapmoveview/CircleLineTapMoveView.kt
+++ circlelinetapmoveview/src/main/java/ui/anwesome/com/circlelinetapmoveview/CircleLineTapMoveView.kt
@@ -3,3 +3,23 @@ package ui.anwesome.com.circlelinetapmoveview
 /**
  * Created by anweshmishra on 01/03/18.
  */
+import android.graphics.*
+import android.view.*
+import android.content.*
+
+class CircleLineTapMoveView(ctx : Context) : View(ctx) {
+    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+
+}
