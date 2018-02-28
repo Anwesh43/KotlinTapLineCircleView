cfb1174b22e6301e0f5c0a427408d0e162e8afe1
Anwesh43 <anweshthecool0@gmail.com>
2018-03-01 01:27:29 +0530

    adding State class

--- circlelinetapmoveview/src/main/java/ui/anwesome/com/circlelinetapmoveview/CircleLineTapMoveView.kt
+++ circlelinetapmoveview/src/main/java/ui/anwesome/com/circlelinetapmoveview/CircleLineTapMoveView.kt
@@ -21,5 +21,26 @@ class CircleLineTapMoveView(ctx : Context) : View(ctx) {
         }
         return true
     }
-
+    data class State(var j : Int = 0, var dir : Float = 0f) {
+        var scales : Array<Float> = arrayOf(0f, 0f)
+        fun update(stopcb : () -> Unit) {
+            scales[j] += 0.1f * dir
+            if(Math.abs(scales[j]) > 1) {
+                scales[j] =  dir
+                j++
+                if(j == scales.size) {
+                    j = 0
+                    dir = 0f
+                    stopcb()
+                }
+            }
+        }
+        fun startUpdating(startcb : () -> Unit) {
+            if(dir == 0f) {
+                scales = arrayOf(0f, 0f)
+                dir = 1f
+                startcb()
+            }
+        }
+    }
 }
