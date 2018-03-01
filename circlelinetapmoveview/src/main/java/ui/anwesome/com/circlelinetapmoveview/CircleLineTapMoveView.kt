fa59fae132e378dc27a6542b69fcbd00a62d7e2b
Anwesh43 <anweshthecool0@gmail.com>
2018-03-01 21:52:07 +0530

    adding Renderer in View

--- circlelinetapmoveview/src/main/java/ui/anwesome/com/circlelinetapmoveview/CircleLineTapMoveView.kt
+++ circlelinetapmoveview/src/main/java/ui/anwesome/com/circlelinetapmoveview/CircleLineTapMoveView.kt
@@ -9,14 +9,15 @@ import android.content.*
 
 class CircleLineTapMoveView(ctx : Context) : View(ctx) {
     val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = Renderer(this)
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap(event.x, event.y)
             }
         }
         return true
