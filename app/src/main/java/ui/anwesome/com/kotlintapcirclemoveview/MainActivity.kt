183f92486f79405d4384f77abae0b7946f312d40
Anwesh43 <anweshthecool0@gmail.com>
2018-03-01 23:00:59 +0530

    adding fullScreen support

--- app/src/main/java/ui/anwesome/com/kotlintapcirclemoveview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlintapcirclemoveview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.kotlintapcirclemoveview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import ui.anwesome.com.circlelinetapmoveview.CircleLineTapMoveView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         CircleLineTapMoveView.create(this)
+        fullScreen()
     }
+}
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
