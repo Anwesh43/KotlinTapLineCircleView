2fbacb661adb753d24825bfc2068ce8f46bfc69b
Anwesh43 <anweshthecool0@gmail.com>
2018-03-01 21:57:39 +0530

    adding CircleLineTapMoveView in Activity

--- app/src/main/java/ui/anwesome/com/kotlintapcirclemoveview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlintapcirclemoveview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlintapcirclemoveview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.circlelinetapmoveview.CircleLineTapMoveView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CircleLineTapMoveView.create(this)
     }
 }
