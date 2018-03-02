61203de199157937090cd21bca99815acb536bf2
Anwesh43 <anweshthecool0@gmail.com>
2018-03-02 06:08:28 +0530

    adding OnMoveListener in Activity

--- app/src/main/java/ui/anwesome/com/kotlintapcirclemoveview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlintapcirclemoveview/MainActivity.kt
@@ -1,19 +1,25 @@
 package ui.anwesome.com.kotlintapcirclemoveview
 
+import android.graphics.PointF
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import ui.anwesome.com.circlelinetapmoveview.CircleLineTapMoveView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        CircleLineTapMoveView.create(this)
+        val view = CircleLineTapMoveView.create(this)
         fullScreen()
+        view.addOnCircleMoveListener { point1, point2 ->
+            Toast.makeText(this,"moved from ${point1.toXYString()} to ${point2.toXYString()}", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 fun MainActivity.fullScreen() {
     supportActionBar?.hide()
     window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
+fun PointF.toXYString():String = "${Math.floor(x.toDouble())}, ${Math.floor(y.toDouble())}"
